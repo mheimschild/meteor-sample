51120d49e58dfc31054b9946b0b95a173ce24972
Milan Heimschild <milan@heimschild.com>
2013-03-21 20:21:12 +0100

    foundation + db

--- meteor-sample.js
+++ meteor-sample.js
@@ -4,6 +4,32 @@ if (Meteor.isClient) {
   Template.tags.tags = function () {
     return tags.find();
   };
+
+  Template.tags.events({
+    'click .edit': function(e) {
+      Session.set('id', e.srcElement.dataset.id);
+      $("#my-modal").toggleClass("hidden");
+    }
+  });
+
+  Template.modal.tag = function() {
+    return tags.findOne(Session.get('id'));
+  };
+
+  Template.modal.selected = function(text) {
+    console.log(text);
+  }
+
+  Template.modal.events({
+    "click #save": function() {
+      var name = $("#name").val();
+      tags.update(Session.get('id'), {name: name});
+      $("#my-modal").toggleClass("hidden"); 
+    },
+    "click #cancel": function() {
+      $("#my-modal").toggleClass("hidden"); 
+    }
+  });
 }
 
 if (Meteor.isServer) {
